49fd0bf17cbbe3246e27d3098a04c4df8e5b2207
Sarang Suresh <sarangsuresh531@gmail.com>
2021-09-13 12:49:51 +0530

    set the Header and background color

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ Your code goes in this file
 --------------------------------------------------------------
 */
 let root = document.querySelector("#root");
+let newHeading = document.querySelector('div');
+newHeading.textContent = "BLING!";
+newHeading.style.cssText = "color : black; font-size:20px; text-align: center; padding-top:20px; font-weight: bold; text-shadow: 2px 2px 2px #CE5937; font-family:sans-serif;"
+let bodyColor = document.querySelector("body");
+bodyColor.style.backgroundColor="#f2ca16";
